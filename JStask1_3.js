2f575750aed341c8c346655c0352f5af49291b51
Ayaka Ohigashi <a.ohigashi.0129@gmail.com>
2023-10-11 00:07:52 +0900

    second commit

--- JStask1_3.js
+++ JStask1_3.js
@@ -1,3 +1,27 @@
-//test
+'use strict';
 
+function showTitle(){
+    console.log('=====================');
+    console.log('現在持っているタスクの一覧');
+    console.log('=====================');
+}
 
+showTitle();
+
+const tasks = [
+    '掃除',
+    '買い物',
+    '散歩',
+];
+
+for (let i = 0; i < tasks.length; i++) {
+    console.log(i + '：' + tasks[i]);
+}
+
+tasks.push(prompt('タスクを入力してください'));
+
+showTitle();
+
+for (let i = 0; i < tasks.length; i++) {
+    console.log(i + '：' + tasks[i]);
+}
